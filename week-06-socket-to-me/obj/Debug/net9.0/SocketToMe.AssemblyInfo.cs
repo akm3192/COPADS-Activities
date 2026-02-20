38733fb13b71c53e8182295dd80de83649e7715a
akm3192 <akm3192@rit.edu>
2026-02-20 10:25:47 -0500

    changes

--- week-06-socket-to-me/obj/Debug/net9.0/SocketToMe.AssemblyInfo.cs
+++ week-06-socket-to-me/obj/Debug/net9.0/SocketToMe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocketToMe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a05e9d6daa06123eff4f7347112d918554ec222")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6dfded4a340c0580e25a00a1284cbe3aa05f86d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocketToMe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocketToMe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
